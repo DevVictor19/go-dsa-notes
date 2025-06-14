4ac15146055af4b585c96a20e1b93d67b8b25d39
DevVictor19 <antoniovictor12@live.com>
2025-06-14 10:56:33 -0300

    feat: add linear search  examples

--- cmd/main.go
+++ cmd/main.go
@@ -7,8 +7,12 @@ import (
 )
 
 func main() {
-	// Binary Search Examples
 	arr := []int{2, 5, 8, 12, 16, 23, 38, 56, 72, 91}
+
+	// Binary Search Examples
 	fmt.Println(search.BinarySearch(arr, 2))
 	fmt.Println(search.BinarySearchRecursive(arr, 0, len(arr)-1, 23))
+
+	// Linear Search Examples
+	fmt.Println(search.LinearSearch(arr, 5))
 }
---
+++ search/linear.go
@@ -0,0 +1,41 @@
+package search
+
+/*
+
+# Linear Search Algorithm
+
+In Linear Search, we iterate over all the elements of the array and check if it the
+current element is equal to the target element.
+
+If we find any element to be equal to the target element, then return the index of
+the current element. Otherwise, if no element is equal to the target element, then
+return -1 as the element is not found.
+
+## Time and complexity
+- best case: O(1)
+- worst case: O(n)
+- average case: O(n)
+
+## Auxiliary space
+- O(1) as except for the variable to iterate through the list, no other variable is used.
+
+## Applications of Linear Search Algorithm:
+- Unsorted Lists
+- Small Data Sets
+- Searching Linked Lists
+- Simple Implementation
+
+## Disadvantages of Linear Search Algorithm:
+- Linear search has a time complexity of O(N), which in turn makes it slow for large datasets.
+- Not suitable for large arrays.
+
+*/
+
+func LinearSearch(arr []int, target int) int {
+	for i, v := range arr {
+		if v == target {
+			return i
+		}
+	}
+	return -1
+}
